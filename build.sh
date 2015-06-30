c847200841102b2c13147464876bf28e7a8a9cab
kentquirk <kentquirk@gmail.com>
2015-06-30 08:48:51 -0400

    improved build script

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
+OUTPUTFILE=build/vasco
+
 mkdir -p build
-go get -d
-go build -o build/vasco
-cp start.sh build
+if go get -d; then
+    if go build -o $OUTPUTFILE; then
+        cp start.sh build
+    else
+        echo "build failed"
+        exit 1
+    fi
+else
+    echo "get failed"
+    exit 1
+fi
+
