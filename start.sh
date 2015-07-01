c084bedec7435789e663163451347eb8de8c4718
kentquirk <kentquirk@gmail.com>
2015-07-01 15:54:25 -0400

    run task in background (oops)

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # vasco expects environment with VASCO_LOCAL for the command port,
 # and VASCO_QUERY for the query port
 
-nohup ./vasco --swagger=$USE_SWAGGER >>$LOGFILE 2>&1
-echo $!>$PIDFILE
+nohup ./vasco --swagger=$USE_SWAGGER >>$LOGFILE 2>&1 &
+echo $! >$PIDFILE
