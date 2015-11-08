bf23cf163ae626c239678534ef22bc64b43f4673
kentquirk <kentquirk@gmail.com>
2015-11-08 13:14:13 -0500

    Don't try to start with swagger

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # vasco expects environment with VASCO_REGISTRY for the command port,
 # VASCO_STATUS for the status port, and VASCO_PROXY for the proxy port
 
-nohup ./vasco --swagger=$USE_SWAGGER >>$LOGFILE 2>&1 &
+nohup ./vasco >>$LOGFILE 2>&1 &
 echo $! >$PIDFILE
